6509115f62a70b5eaf5cba369216054e05a6b7a1
andypratama3 <andypratama1211@gmail.com>
2024-04-10 06:01:34 +0800

    Done Set Content Privacy Police

--- resources/views/layouts/dashboard/partial/script.blade.php
+++ resources/views/layouts/dashboard/partial/script.blade.php
@@ -1,4 +1,5 @@
-<script src="{{ asset('asset_dashboard/vendor/jquery/jquery.min.js') }}"></script>
+{{-- <script src="{{ asset('asset_dashboard/vendor/jquery/jquery.min.js') }}"></script> --}}
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script src="{{ asset('asset_dashboard/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ asset('asset_dashboard/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 <script src="{{ asset('asset_dashboard/js/ruang-admin.min.js') }}"></script>
