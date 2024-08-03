60799c548d4aac2e4407d64227c2fc9b3c627e55
andypratama3 <andypratama1211@gmail.com>
2024-08-04 03:55:34 +0800

    update secure3

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -27,4 +27,17 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    // app/Exceptions/Handler.php
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof \Illuminate\Database\Eloquent\ModelNotFoundException) {
+            // Customize handling for ModelNotFoundException
+            return response()->view('errors.404', [], 404);
+        }
+
+        return parent::render($request, $exception);
+    }
+
 }
