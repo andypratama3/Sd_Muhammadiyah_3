4b4bef43fd17568944db5c20c65ee000ed419f26
andypratama3 <andypratama1211@gmail.com>
2025-02-26 12:40:31 +0800

    update migration comment

--- database/migrations/2023_11_02_114138_create_comments_table.php
+++ database/migrations/2023_11_02_114138_create_comments_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->longText('comment')->nullable()->default('text');
+            $table->longText('comment')->nullable();
             $table->string('slug');
             $table->timestamps();
         });
