a603d330b7f9e1225c4b09cf4166db97e50c361e
andypratama3 <andypratama1211@gmail.com>
2025-03-07 20:36:49 +0800

    Fix Seo

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('app:sync-wilayah-command')->yearly();
-        $schedule->command('app:site-map-command')->everyDay();
+        $schedule->command('app:site-map-command')->sundays();
         // $schedule->command('app:charge-payment')->monthly();
         // $schedule->command('app:check-transaction-old')->monthly();
         // $schedule->command('app:archive-charges')->yearly();
