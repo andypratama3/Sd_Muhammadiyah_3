9b90e0387b5dacd0d7849ca562d62d7d044c385c
andypratama3 <andypratama1211@gmail.com>
2025-03-24 02:41:12 +0800

    View: Berita & se

--- resources/views/berita/detail-berita.blade.php
+++ resources/views/berita/detail-berita.blade.php
@@ -5,13 +5,13 @@
     <meta name="description" content="{{ Str::limit(strip_tags($berita->desc), 160) }}">
     <meta name="keywords" content="Berita, {{ $berita->judul }}, Informasi Terbaru">
     <meta property="og:title" content="{{ $berita->judul }}">
-    <meta property="og:description" content="{{ Str::limit(strip_tags($berita->desc), 160) }}">
+    <meta property="og:description" content="{!! $berita->desc !!}">
     <meta property="og:image" content="{{ asset('storage/img/berita/'. $berita->foto) }}">
     <meta property="og:url" content="{{ url()->current() }}">
     <meta property="og:type" content="article">
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:title" content="{{ $berita->judul }}">
-    <meta name="twitter:description" content="{{ Str::limit(strip_tags($berita->desc), 160) }}">
+    <meta name="twitter:description" content="{!! $berita->desc !!}">
     <meta name="twitter:image" content="{{ asset('storage/img/berita/'. $berita->foto) }}">
 @endpush
 
