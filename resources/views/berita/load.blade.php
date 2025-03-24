d9f3adda411a6c7d4e207f852bb8884d6ea9063f
andypratama3 <andypratama1211@gmail.com>
2025-03-24 23:03:16 +0800

    View: Berita & se

--- resources/views/berita/load.blade.php
+++ resources/views/berita/load.blade.php
@@ -13,7 +13,7 @@
                 <div class="small"><span class="fa fa-calendar text-primary"></span>
                     {{ \Carbon\Carbon::parse($berita->created_at)->diffForHumans() }}</div>
             </div>
-            <a href="{{ route('berita.show', $berita->slug) }}" class="h4 d-inline-block mb-3">{{ $berita->judul }}</a>
+            <a href="{{ route('berita.show', $berita->slug) }}" class="h4 d-inline-block mb-3">{{ Str::limit($berita->judul, 50) }}</a>
             <p class="mb-3">{!! Str::substr($berita->desc, 0, 50) !!}</p>
             <a href="{{ route('berita.show', $berita->slug) }}" class="btn p-0">Lihat Berita  <i class="fa fa-arrow-right"></i></a>
         </div>
