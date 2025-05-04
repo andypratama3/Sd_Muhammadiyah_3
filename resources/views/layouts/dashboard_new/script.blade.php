90a480c70fd207a6a770cc6c64f0e8bfcdedc8ce
andypratama3 <andypratama1211@gmail.com>
2025-05-05 00:44:56 +0800

    Fix Feat: Gallery Action

--- resources/views/layouts/dashboard_new/script.blade.php
+++ resources/views/layouts/dashboard_new/script.blade.php
@@ -20,6 +20,8 @@
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <!-- GitHub buttons -->
 <script src="{{ asset('asset_dashboard/vendor/select2/dist/js/select2.js') }}"></script>
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<script src="{{ asset('asset_dashboard/vendor/select2/dist/js/select2.js') }}"></script>
 
 <script async defer src="https://buttons.github.io/buttons.js"></script>
 
