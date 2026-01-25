bb83f6adbcbf6ac5148f26cb6f29e344f49dbca4
andypratama3 <andypratama1211@gmail.com>
2026-01-26 03:22:11 +0800

    Update Map

--- routes/web.php
+++ routes/web.php
@@ -230,7 +230,7 @@ Route::group(['prefix' => 'dashboard', 'middleware' => ['auth', 'verified']], fu
         Route::post('/masuk', [AbsensiController::class, 'absenMasuk'])->name('absensi.masuk');
         Route::post('/pulang', [AbsensiController::class, 'absenPulang'])->name('absensi.pulang');
         Route::get('/riwayat', [AbsensiController::class, 'riwayat'])->name('absensi.riwayat');
-        Route::get('/kml/data', [AbsensiController::class, 'getKmlData'])->name('absensi.kml.data');
+        // Route::get('/kml/data', [AbsensiController::class, 'getKmlData'])->name('absensi.kml.data');
 
     });
 
