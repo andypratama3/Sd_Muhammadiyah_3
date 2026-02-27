7be50700d569260e502059c24ddb392c021b36f5
andypratama3 <andypratama1211@gmail.com>
2026-02-28 01:14:24 +0800

    Update for back

--- app/Services/AbsensiService.php
+++ app/Services/AbsensiService.php
@@ -684,7 +684,7 @@ class AbsensiService
         if ($this->isRoleTerbatas($jenisPegawai) && $absensi->jamKerja) {
             $batasPulang  = Carbon::parse($absensi->jamKerja->batas_pulang, 'Asia/Makassar');
             $statusPulang = $now->lessThan($batasPulang) ? 'pulang_cepat' : 'tepat_waktu';
-            $rp_pulang    = ($this->isRoleDapatPoin($jenisPegawai) && $statusPulang === 'tepat_waktu') ? 4000 : 4000;
+            $rp_pulang    = ($this->isRoleDapatPoin($jenisPegawai) && $statusPulang === 'tepat_waktu') ? 4000 : 0;
 
         } elseif ($absensi->jamKerja && $absensi->jamKerja->batas_pulang) {
             $batasPulang  = Carbon::parse($absensi->jamKerja->batas_pulang, 'Asia/Makassar');
